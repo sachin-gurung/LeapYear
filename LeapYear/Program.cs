c30f2fcd082f05ce33a49391de81828179032fc3
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:29:53 -0800

    First Commit

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
+﻿
+/*
+ * C# Program to Check Whether the Entered Year is a Leap Year or Not
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace LeapYear
+ 
+namespace Program
 {
-    class Program
+    class leapyear
     {
         static void Main(string[] args)
         {
+            leapyear obj = new leapyear();
+            obj.readdata();
+            obj.leap();
+        }
+        int y;
+        public void readdata()
+        {
+            Console.WriteLine("Enter the Year in Four Digits : ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public void leap()
+        {
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", y);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", y);
+            }
+            Console.ReadLine();
         }
     }
 }
+/*
+ Result:
+ Enter the Year in Four Digits : 1004
+1004 is a Leap Year
+*/
